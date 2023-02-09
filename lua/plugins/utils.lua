4cf842e266fafc83efeeeeac87ed1b77915b459a
CHEN Zhongpu <chenloveit@gmail.com>
2023-02-09 18:54:35 +0800

    Enable neo-tree to watch file changes
    Signed-off-by: CHEN Zhongpu <chenloveit@gmail.com>

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -12,10 +12,11 @@ return {
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = true,
+        use_libuv_file_watcher = true,
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = false,
-          hide_gitignored = true,
+          hide_gitignored = false,
         },
       },
     },
