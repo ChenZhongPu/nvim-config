f7ca6d25f1e25d48c21e3ccfcfe1d013fabe7391
CHEN Zhongpu <chenloveit@gmail.com>
2023-02-10 18:41:59 +0800

    Add a key mapping for toggle terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Togger a terminal
+vim.keymap.set("n", "<leader>T", ":ToggleTerm<CR>", { desc = "Toggle A Terminal" })
