b1530e340469646ed3507427287a50615f90df43
CHEN zhongpu <chenloveit@gmail.com>
2023-04-06 10:51:09 +0800

    surrond, and Tab
    - disable tab for cmp
    - use mini.surround

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -28,7 +28,7 @@ return {
   -- doc string
   {
     "danymat/neogen",
-    ft = { "python", "rust" },
+    ft = { "python", "rust", "c" },
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = true,
   },
@@ -66,47 +66,6 @@ return {
           }),
         },
       }
-    end,
-  },
-  -- disable mini.surround
-  {
-    "echasnovski/mini.surround",
-    enabled = false,
-  },
-  -- remove surround key mapping
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      wk.register({
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>b"] = { name = "+buffer" },
-        ["<leader>c"] = { name = "+code" },
-        ["<leader>f"] = { name = "+file/find" },
-        ["<leader>g"] = { name = "+git" },
-        ["<leader>gh"] = { name = "+hunks" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>s"] = { name = "+search" },
-        ["<leader>sn"] = { name = "+noice" },
-        ["<leader>u"] = { name = "+ui" },
-        ["<leader>w"] = { name = "+windows" },
-        ["<leader>x"] = { name = "+diagnostics/quickfix" },
-      })
-    end,
-  },
-  -- add nvim-surround
-  {
-    "kylechui/nvim-surround",
-    lazy = true,
-    event = "BufRead",
-    config = function()
-      require("nvim-surround").setup({})
     end,
   },
 }
