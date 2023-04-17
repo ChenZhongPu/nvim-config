3b308526cbeb08cc8a545be56b100fac4e726df7
CHEN zhongpu <chenloveit@gmail.com>
2023-04-17 09:27:10 +0800

    blog tex template

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -16,5 +16,11 @@ vim.keymap.set(
   "n",
   "<leader>ip",
   ":.-1read " .. texPath .. "/project.tex<CR>",
-  { desc = "Import Project Tex Template" }
+  { desc = "🔖Import Project (Homework) Tex Template" }
 )
+
+vim.keymap.set("n", "<leader>ib", function()
+  os.execute("cp " .. texPath .. "/.latexmkrc " .. vim.fn.expand("%:p:h") .. "/.latexmkrc")
+  os.execute("cp " .. texPath .. "/blog.sty " .. vim.fn.expand("%:p:h") .. "/blog.sty")
+  vim.cmd(".-1read " .. texPath .. "/blog.tex")
+end, { desc = "✍Import Blog Tex Package" })
