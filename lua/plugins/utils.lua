1ec6ba9fd8a074d924d7226421ba2bb75393cb96
CHEN Zhongpu <chenloveit@gmail.com>
2023-04-21 19:41:35 +0800

    copilot icon from lazy

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -44,13 +44,13 @@ return {
   --     end)
   --   end,
   -- },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, function()
-        return "🤖" .. require("copilot.api").status.data.status
-      end)
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sections.lualine_x, function()
+  --       return "🤖" .. require("copilot.api").status.data.status
+  --     end)
+  --   end,
+  -- },
 }
