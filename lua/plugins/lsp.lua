cb4091e1dccd19c7b6eb66671db96ab51848aa86
CHEN zhongpu <chenloveit@gmail.com>
2023-06-21 16:58:04 +0800

    fix bugs of latex config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,9 +31,9 @@ return {
       vim.g.vimtex_quickfix_open_on_warning = 0
       if global.is_mac then
         vim.g.vimtex_view_method = "skim"
-        -- vim.g.vimtex_view_skim_sync = 1
-        -- vim.g.vimtex_view_skim_activate = 1
-        -- vim.g.vimtex_view_reading_bar = 1
+        vim.g.vimtex_view_skim_sync = 1
+        vim.g.vimtex_view_skim_activate = 1
+        vim.g.vimtex_view_skim_reading_bar = 1
       end
       if global.is_linux then
         vim.g.vimtex_view_method = "zathura"
