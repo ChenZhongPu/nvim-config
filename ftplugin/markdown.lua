d55fcf5240a783d469cbfe22880f1683f4e1f402
CHEN Zhongpu <chenloveit@gmail.com>
2026-05-21 18:51:48 +0800

    add link color for markdown->pdf

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -7,21 +7,39 @@ opt.spell = true
 opt.spelllang = { "en_us", "cjk" }
 
 local mdPath = vim.fn.stdpath("config") .. "/third-party/markdown/head.tex"
-local fullPath = vim.fn.expand("%:p")
-local fullPathWithoutType = vim.fn.expand("%:p:r")
+
+local function shell_join(args)
+  return table.concat(
+    vim.tbl_map(function(arg)
+      return vim.fn.shellescape(arg)
+    end, args),
+    " "
+  )
+end
 
 vim.keymap.set("n", "<leader>mp", function()
   vim.api.nvim_command("write")
-  local command = string.format(
-    "pandoc --pdf-engine=xelatex --syntax-highlighting zenburn -H %s %s -o %s-export.pdf",
+  local fullPath = vim.fn.expand("%:p")
+  local fullDir = vim.fn.expand("%:p:h")
+  local fullPathWithoutType = vim.fn.expand("%:p:r")
+  local resourcePathSeparator = vim.fn.has("win32") == 1 and ";" or ":"
+  local resourcePath = table.concat({ fullDir, vim.fn.getcwd() }, resourcePathSeparator)
+  local command = shell_join({
+    "pandoc",
+    "--pdf-engine=xelatex",
+    "--syntax-highlighting",
+    "zenburn",
+    "--resource-path=" .. resourcePath,
+    "-H",
     mdPath,
     fullPath,
-    fullPathWithoutType
-  )
-  local success = os.execute(command)
-  if success then
+    "-o",
+    fullPathWithoutType .. "-export.pdf",
+  })
+  local output = vim.fn.system(command)
+  if vim.v.shell_error == 0 then
     print("Export Markdown to PDF successfully!")
   else
-    print("Failed to export Markdown to PDF.")
+    vim.notify("Failed to export Markdown to PDF.\n" .. output, vim.log.levels.ERROR)
   end
-end, { desc = "Pandoc PDF Export" })
+end, { buffer = true, desc = "Pandoc PDF Export" })
